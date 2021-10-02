fb300cae340670e8c3ed71c540460967bd65fb2e
lisoenot <dgorohov80@gmail.com>
2021-10-02 13:34:20 +0300

    Migrate backend 3.1 to 5.0

---
+++ backend_v-choice/backend_v-choice/Controllers/FilmController.cs
@@ -0,0 +1,111 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BLL.DTO;
+using BLL.Interface;
+
+namespace backend_v_choice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : Controller
+    {
+        private readonly ICrudService _crudService;
+        private readonly IPaginationService _paginationService;
+        private readonly ILogger _logger;
+
+        public FilmController(ICrudService cs, IPaginationService ps, ILogger<FilmController> logger)
+        {
+            _crudService = cs;
+            _paginationService = ps;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFilmsPagination([FromQuery] PaginationQuery query)
+        {
+            _logger.LogInformation("Get pagination films");
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("Get pagination films: model state is not valid.");
+                
+                return BadRequest(ModelState);
+            }
+
+            var res = await _paginationService.GetFilmsPagination(query);
+            if (res == null) return StatusCode(500);
+
+            return Ok(res);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFilm([FromRoute] int id)
+        {
+            _logger.LogInformation($"Get film with Id equal {id}.");
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("Get film: model state is not valid.");
+                
+                return BadRequest(ModelState);
+            }
+
+            var film = await _crudService.GetFilmAsync(id);
+            if (film == null) return NotFound();
+
+            return Ok(film);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] FilmDTO film)
+        {
+            _logger.LogInformation("Create film.");
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("Create film: model state is not valid.");
+                
+                return BadRequest(ModelState);
+            }
+
+            var filmDTO = await _crudService.CreateFilmAsync(film);
+            if (filmDTO == null) return StatusCode(500);
+
+            return CreatedAtAction("CreateFilm", new { id = film.Id }, film);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] FilmDTO film)
+        {
+            _logger.LogInformation($"Update film with Id equal {id}.");
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("Update film: model state is not valid.");
+                
+                return BadRequest(ModelState);
+            }
+
+            await _crudService.UpdateFilmAsync(id, film);
+            
+            return NoContent();
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            _logger.LogInformation($"Delete film with Id equal {id}.");
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("Delete film: model state is not valid.");
+                
+                return BadRequest(ModelState);
+            }
+
+            await _crudService.DeleteFilmAsync(id);
+            
+            return NoContent();
+        }
+    }
+}
---
+++ backend_v-choice/backend_v-choice/Startup.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using BLL.Interface;
+using BLL.Service;
+using BLL.Utils;
+using DAL;
+using DAL.Interface;
+using DAL.Model;
+using DAL.Repository;
+
+namespace backend_v_choice
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "backend_v_choice", Version = "v1" });
+            });
+            
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DBContext>(opt => opt.UseSqlServer(connection));
+            services.AddScoped<IFilmRepository, FilmRepository>();
+            services.AddScoped<IGenreRepository, GenreRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ICommentsRepository, CommentsRepository>();
+            services.AddScoped<IAutorizationService, AutorizationService>();
+            services.AddScoped<ICrudService, CrudService>();
+            services.AddScoped<IPaginationService, PaginationService>();
+            services.AddScoped<IFavoriteService, FavoriteService>();
+            services.AddScoped<IMapper, Mapper>();
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<DBContext>();
+           
+            services.AddMvc().AddNewtonsoftJson(o =>
+            {
+                o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+
+            services.AddCors();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "ViewerChoiceApp";
+                options.LoginPath = "/";
+                options.AccessDeniedPath = "/";
+                options.LogoutPath = "/";
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                };
+                options.Events.OnRedirectToAccessDenied = context =>
+                {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                };
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backend_v_choice v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseStaticFiles();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+        }
+
+        private async Task CreateUserRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+            // Создание ролей администратора и пользователя
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+
+            // Создание Администратора
+            string adminEmail = "admin@mail.com";
+            string adminPassword = "Admin21!";
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+
+            // Создание Пользователя
+            string userEmail = "user@mail.com";
+            string userPassword = "User21!";
+            if (await userManager.FindByNameAsync(userEmail) == null)
+            {
+                User user = new User { Email = userEmail, UserName = userEmail };
+                IdentityResult result = await userManager.CreateAsync(user, userPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "user");
+                }
+            }
+        }
+    }
+}
